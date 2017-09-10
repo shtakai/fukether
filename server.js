45911fbffbd4e66255d2e96127977a24f49c785d
Alyson T <shtakai@gmail.com>
2017-09-10 19:53:49 +0900

    Add hbs partial

--- server.js
+++ server.js
@@ -4,22 +4,24 @@ const hbs = require('hbs');
 const app = express();
 const path = require('path');
 
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, '/public')));
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', text => text.toUpperCase());
 
 app.get('/', (req, res) => {
   // res.send('<h1>crazed fucked up daily life</h1>');
   res.render('home.hbs', {
     pageTitle: 'Home Page',
     welcomeMessage: 'Welcome to my website',
-    currentYear: new Date().getFullYear(),
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
-    currentYear: new Date().getFullYear(),
   });
 });
 
