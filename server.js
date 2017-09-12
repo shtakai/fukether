0e9bca326bcce8fe89475311de02bd208812b9eb
Alyson T <shtakai@gmail.com>
2017-09-12 21:02:49 +0900

    express middleware

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 const app = express();
 const path = require('path');
 
 hbs.registerPartials(path.join(__dirname, '/views/partials'));
 app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, '/public')));
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', `${log}\n`, (err) => {
+    if (err) {
+      console.log('unnable to append to server.log');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
 
 hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
 hbs.registerHelper('screamIt', text => text.toUpperCase());
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
   // res.send('<h1>crazed fucked up daily life</h1>');
